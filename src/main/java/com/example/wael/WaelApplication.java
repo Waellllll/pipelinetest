7661189e513a2d3b497a3dc9df9eb8d1439dfd16
waellllll <wael.khalfi@esprit.tn>
2025-12-05 13:50:13 +0100

    pp

--- src/main/java/com/example/wael/WaelApplication.java
+++ src/main/java/com/example/wael/WaelApplication.java
@@ -1,13 +1,13 @@
-package tn.esprit.spring;
+package com.example.wael;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TimesheetDevopsApplication {
+public class WaelApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TimesheetDevopsApplication.class, args);
+		SpringApplication.run(WaelApplication.class, args);
 	}
 
 }
---
+++ src/test/java/com/example/wael/WaelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wael;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
